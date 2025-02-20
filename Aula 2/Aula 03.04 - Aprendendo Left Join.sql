a8f7532d6983ab70c9447434e85c13804e75285d
Breno Silva Rubio <108412937+brenosrubio@users.noreply.github.com>
2025-02-20 13:38:36 -0300

    Update Aula 03.04 - Aprendendo Left Join.sql

--- Aula 2/Aula 03.04 - Aprendendo Left Join.sql
+++ Aula 2/Aula 03.04 - Aprendendo Left Join.sql
@@ -21,3 +21,27 @@ SELECT *
 FROM clientes c
 JOIN 
 pedidos p;
+
+
+/*
+Conhecemos até agora duas cláusulas, tanto a INNER JOIN como a RIGHT JOIN. E se usássemos aqui a RIGHT JOIN, estaríamos passando as nossas tabelas de forma inversa,
+porque o cliente teria que estar após a cláusula RIGHT JOIN para ser considerado a nossa tabela da direita. 
+Resolveríamos de forma simples, só precisaríamos inverter a posição das duas tabelas.
+
+Mas temos outra cláusula JOIN que podemos usar aqui, que é a LEFT JOIN. E ela é bem semelhante à cláusula RIGHT JOIN. 
+A diferença entre uma e outra é que com a RIGHT JOIN trazemos todos os dados da tabela da direita e apenas os que correspondem à tabela da esquerda, e com a LEFT JOIN, 
+traremos todos os dados da tabela da esquerda e apenas os que correspondem à nossa tabela da direita.
+
+Então, usando o LEFT JOIN, podemos deixar o cliente do lado esquerdo. Com o comando ON, vamos trazer, de clientes, o ID, ou seja, c.id, e da 
+tabela de pedidos o p.idcliente.
+
+Também vamos especificar o nome do nosso cliente e o id do nosso pedido: c.nome, p.id.
+*/
+
+
+SELECT * FROM clientes;
+ 
+SELECT c.nome, p.id
+FROM clientes c 
+LEFT JOIN Pedidos p 
+ON c.id = p.idcliente;
